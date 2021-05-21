39f56dd500d2a2eb2ca03b2ac378df4ffeebe6b2
riliks <72131460+riliks@users.noreply.github.com>
2021-05-21 14:11:09 +0300

    Update mytcpserver.cpp

--- serverQt/mytcpserver.cpp
+++ serverQt/mytcpserver.cpp
@@ -13,6 +13,7 @@ MyTcpServer::~MyTcpServer()
     }
     server_status=0;
     mTcpServer->close();
+    db.close();
 }
 MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
     mTcpServer = new QTcpServer(this);
@@ -24,6 +25,13 @@ MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
         server_status=1;
         qDebug() << "server is started";
     }
+    
+    db = QSqlDatabase::addDatabase("QPSQL");
+    db.setHostName("127.0.0.1");
+    db.setDatabaseName("seabattle");
+    db.setUserName("postgres");
+    db.setPassword("****");// здесь поставить свой пароль от базы данных, который вводится при запуске
+    db.open();
 }
 
 void MyTcpServer::slotNewConnection(){
@@ -131,17 +139,35 @@ void MyTcpServer::slotServerRead(){
         int pos1 = message.find("6");                               // 2 - ранил
         action = message.substr(pos+1, message.length() - 3);       // 3 - убил
         message.erase(0);                                           // 4 - корабль
-                                                                    // 5 - остров
+        QSqlQuery que;                                              // 5 - остров
                                                                     // 6 - отметка пользователя
         switch (field[pos1]) {
             case 0:
                 field[pos1] = 1;
+               if (number=2)
+                que.exec("update users set shoots=shoots+1 where number =2"); // пустые выстрелы
+                number=1;
+                else
+                que.exec("update users set shoots=shoots+1 where number =1");
+                number=2;
                 break;
             case 4:
                 if (field[pos1 - 1] == 4 || field[pos1 + 1] == 4)
                     field[pos1] = 2;
+                 if (number=2)
+                que.exec("update users set shoots=shoots+1,aim=aim+1 where number =2"); // попадание
+                number=1;
+                else
+                que.exec("update users set shoots=shoots+1,aim=aim+1 where number =1");
+                number=2;
                 else
                     field[pos1] = 3;
+                if (number=2)
+                que.exec("update users set shoots=shoots+1,aim=aim+1,ships=ships+1 where number =2"); // убил
+                number=1;
+                else
+                que.exec("update users set shoots=shoots+1,aim=aim+1,ships=ships+1 where number =1");
+                number=2;
                 break;
 
         int j = 0;
